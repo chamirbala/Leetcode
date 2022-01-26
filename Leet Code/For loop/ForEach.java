19d253f45a249c29e0b4836d401d2c3378226de0
chamirbala <81492799+chamirbala@users.noreply.github.com>
2022-01-26 09:45:56 +0530

    Add files via upload

---
+++ Leet Code/For loop/ForEach.java
@@ -0,0 +1,11 @@
+class ForEach{
+    public static void main(String[]args){
+        int[] a={0,1,2,3,7,8};
+        int sum=0;
+        for(int i:a){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+
+}
